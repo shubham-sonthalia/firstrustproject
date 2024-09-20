7bfdb939d4e6ea9592080843966c34eedca52b01
Shubham Sonthalia <shubham18366@iiitd.ac.in>
2024-09-20 19:23:00 +0530

    using Box instead of lifetimes and Enums instead of modifying mutable reference

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,41 @@
 use std::{time::Duration, thread::sleep};
 
+enum UpdateResult {
+    Continue,
+    QuitApplication
+}
+
 fn main() {
-    let mut client = MyClient {ticks_left : 4};
+    let client = Box::new(MyClient {ticks_left : 4});
     let mut app = App {
         title: "My First Rust Window App".to_string(),
-        running: true,
-        client: &mut client,
+        client: client,
     };
     app.run();
 }
 // Rust doesn't have classes. 
 // Lifetimes and Traits (things to learn about)
 
-struct App<'a> {
+struct App{
     title:String,
-    running:bool,
     // whenever we use a trait as a property of a struct, need to give dyn keyword
     // to tell the compiler that the actual type will be decided at runtime.;
-    client: &'a mut dyn Client,
+    //client: &mut dyn Client,
+    client:Box<dyn Client>
 }
 struct MyClient {
     ticks_left: usize,
 }
-impl App<'_> {
+impl App {
     fn run (&mut self){
         println!("=== You are now playing {} ===", self.title);
         loop {
-            self.client.update(self);
+            let update_result = self.client.update();
             self.client.render();
-
-            if !self.running {
-                break;
+            
+            match update_result {
+                UpdateResult::QuitApplication => {return;},
+                UpdateResult::Continue => {}   
             }
             sleep(Duration::from_secs(1));
         }
@@ -38,16 +43,17 @@ impl App<'_> {
 }
 
 trait Client {
-    fn update(&mut self, app: &mut App);
+    fn update(&mut self) -> UpdateResult;
     fn render(&self);
 }
 
 impl Client for MyClient {
-    fn update(&mut self, app: &mut App){
+    fn update(&mut self) -> UpdateResult{
         self.ticks_left -=1; 
         if self.ticks_left == 0 {
-            app.running = false;
+            return UpdateResult::QuitApplication;
         }
+        return UpdateResult::Continue;
     }
     fn render(&self){
         if self.ticks_left > 0 {
