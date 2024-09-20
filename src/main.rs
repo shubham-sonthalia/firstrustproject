7b46ff64e90adde140f218e7c0477f390c4129ac
Shubham Sonthalia <shubham18366@iiitd.ac.in>
2024-09-20 17:24:03 +0530

    next changes

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,63 @@
 use std::{time::Duration, thread::sleep};
 
 fn main() {
+    let mut client = MyClient {ticks_left : 4};
     let mut app = App {
         title: "My First Rust Window App".to_string(),
-        ticks_left: 4, 
         running: true,
+        client: &mut client,
     };
-    println!("=== You are now playing {} ===", app.title);
-
-    loop {
-        app.update();
-        app.render();
-        // called an associated function using the :: operator
-        // App::awyiin();
-        
-        if !app.running{
-            break;
-        }
-        sleep(Duration::from_secs(1));
-    }
+    app.run();
 }
 // Rust doesn't have classes. 
 // Lifetimes and Traits (things to learn about)
 
-struct App {
+struct App<'a> {
     title:String,
-    ticks_left: usize, 
     running:bool,
+    // whenever we use a trait as a property of a struct, need to give dyn keyword
+    // to tell the compiler that the actual type will be decided at runtime.;
+    client: &'a mut dyn Client,
 }
+struct MyClient {
+    ticks_left: usize,
+}
+impl App<'_> {
+    fn run (&mut self){
+        println!("=== You are now playing {} ===", self.title);
+        loop {
+            self.client.update(self);
+            self.client.render();
 
-impl App {
-
-    // a method because it uses self 'caller'
-    fn update(&mut self) {
-        self.ticks_left -= 1; 
-        if self.ticks_left == 0{
-            self.running = false;
+            if !self.running {
+                break;
+            }
+            sleep(Duration::from_secs(1));
         }
     }
+}
 
-    // a method because it uses self caller.
-    fn render(&self) {
+trait Client {
+    fn update(&mut self, app: &mut App);
+    fn render(&self);
+}
+
+impl Client for MyClient {
+    fn update(&mut self, app: &mut App){
+        self.ticks_left -=1; 
+        if self.ticks_left == 0 {
+            app.running = false;
+        }
+    }
+    fn render(&self){
         if self.ticks_left > 0 {
-            println!("You turn the crank...");
+            println!("You turn the crank ...");
         }
         else{
-            println!("Jack POPS OUT OF THE BOX");
+            println!("jack pops out of the box!!");
         }
     }
-
-    // an associated function which needs to be called with :: and not '.'
-    fn awyiin(){
-        println!("1");
-    }
 }
+
+// impl <struct name> 
+// impl <trait name> for <struct name>
